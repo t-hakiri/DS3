58ce5cfd07c9e38a0613e6cfc64c49ad1a97c9a4
yu shimamura <mashiyu0914@gmail.com>
2020-05-25 22:16:16 +0900

    add: フロント最低限

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -1,2 +1,23 @@
 class RecruitmentsController < ApplicationController
+  def index
+    @recruitments = Recruitment.all
+  end
+
+  def new
+    @recruitment = Task.new
+  end
+
+  def create
+    @recruitment = Recruitment.new(recruitment_params)
+    if @recruitment.save
+      redirect_to recruitment_path, notice: "募集を開始しました"
+    else
+      render :new
+    end
+  end
+
+  private
+  def recruitment_params
+    params.require(:recruitment).permit(:owner_level, :area, :game_model, :secret_word, :lap_coutn)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_143416) do
+ActiveRecord::Schema.define(version: 2020_05_22_145655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "popular_areas", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recruitments", force: :cascade do |t|
     t.integer "owner_level"
