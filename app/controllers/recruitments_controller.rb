91593dda189bda68d7d799c0c44369827a9f9124
yu shimamura <mashiyu0914@gmail.com>
2020-05-27 04:37:11 +0900

    add: モーダルで募集を作れるように

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -18,11 +18,14 @@ class RecruitmentsController < ApplicationController
 
 
   def create
-    @recruitment = Recruitment.new(recruitment_params)
+    # @recruitment = Recruitment.new(recruitment_params)
+    @recruitment = Recruitment.new(owner_level: recruitment_params[:owner_level], area: recruitment_params[:area].to_i, game_model: recruitment_params[:game_model].to_i, secret_word: recruitment_params[:secret_word], lap_count: recruitment_params[:lap_count])
     if @recruitment.save
-      redirect_to recruitment_path, notice: "募集を開始しました"
+      redirect_to recruitments_path, notice: "募集を開始しました"
     else
-      render :new
+      @search = Recruitment.search(params[:q])
+      @recruitments = @search.result.recent
+      render action: :index
     end
   end
 
@@ -45,7 +48,7 @@ class RecruitmentsController < ApplicationController
   private
 
   def recruitment_params
-    params.require(:recruitment).permit(:owner_level, :area, :game_model, :secret_word, :lap_coutn)
+    params.require(:recruitment).permit(:owner_level, :area, :game_model, :secret_word, :lap_count)
   end
 
   def set_recruitment
